c22aa1e55a666d23ebfd909bd67dd1ee2611c2ff
Cesar Ayar <cesar.ayar@gmail.com>
2026-06-14 17:47:52 -0600

    chore: Update Github link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://docs.code-me.club',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -91,7 +91,7 @@ const config = {
             label: 'Manual de Marca',
           },
           {
-            href: 'https://github.com/c',
+            href: 'https://github.com/Code-MeMX',
             label: 'GitHub',
             position: 'right',
           },
